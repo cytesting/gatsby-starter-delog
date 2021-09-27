ddc48e6224584051d1cfa751dafc966e689e45c1
Leonardo Martinez <lowenhard@gmail.com>
2021-09-27 09:47:58 -0500

    Add Netlify function

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,22 +20,20 @@ const ContactPage = ({
           <p>Profesores de álgebra lineal &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
-            <div>
-              <label htmlFor="w3lName">Nombre</label>
-              <input type="text" name="w3lName" id="w3lName"/>
+          <form className="form-container" name="newsletter" method="POST" data-netlify="true" netlify-honeypot="bot-field">
+            <div hidden aria-hidden="true">
+              <label>
+                No rellenes este campo si eres humano: 
+                <input name="bot-field" />
+              </label>
             </div>
             <div>
-              <label htmlFor="w3lSender">Correo</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
+              <label for="email">Correo</label>
+              <input type="email" name="email" id="email" required />
             </div>
             <div>
-              <label htmlFor="w3lSubject">Asunto</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Mensaje</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
+              <label for="message">Mensaje</label>
+              <textarea name="message" id="message"></textarea>
             </div>
             <div style={{display: "flex", justifyContent: "flex-end"}}>
               <input type="submit" className="button -primary" style={{marginRight: 0}} />
